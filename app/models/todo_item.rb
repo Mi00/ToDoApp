53bd3004d6322638c178b57061f92da839aa30be
Mi00 <muturuzi@gmail.com>
2016-01-26 10:58:04 +0100

    second commit

---
+++ app/models/todo_item.rb
@@ -0,0 +1,15 @@
+class TodoItem < ActiveRecord::Base
+  belongs_to :todo_list
+
+  def completed?
+  	!completed_at.blank?
+  end	
+
+  def toggle_completion
+  	if completed?
+  		update_attribute(:completed_at, nil)
+  	else
+  		update_attribute(:completed_at, Time.now)
+  	end
+  end
+end
